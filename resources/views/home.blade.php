9e48d2540cba8757643a43db68f4c8b0e13b8491
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2021-01-19 11:51:24 +0100

    Configuracion

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Hola mundo</h1>
+    <livewire:componente/>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @livewireStyles
+    <script src="{{ url('/js/main.js') }}"></script>
+    @livewireScripts
+    <title>Home</title>
+</head>
+<body>
+    <script src="{{ asset(mix('js/app.js')) }}"></script>
+    @yield('content')
+</body>
+</html>
